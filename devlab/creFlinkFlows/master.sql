5ab39b28e1a295404ca08dcfd6de7cf9556596fd
George Leonard <georgelza@gmail.com>
2025-12-19 20:39:06 +0200

    fixing infrastructure/flink/Dockerfile, re-adding the python module install

--- devlab/creFlinkFlows/master.sql
+++ devlab/creFlinkFlows/master.sql
@@ -1,19 +1,25 @@
 
+-- make cat
 -- Execute script 1: 
 -- Create Catalogs
 -- 1.1
 SOURCE '/creFlinkFlows/scripts/1.1.creCat.sql';
 
+
+-- make crecdc
 -- Execute script 2: 
 -- Create CDC Source Tables
 -- This will always create the demog database as our catalog is generic_in_memory
 -- 2.1
 SOURCE '/creFlinkFlows/scripts/2.1.creCdcDemog.sql';
 
+
+-- make crefinflow
 -- Execute script 3: 
 -- Create our embedding output table
 -- 3.1
 SOURCE '/creFlinkFlows/scripts/3.1.creTargetDemog.sql';
+
 
 -- Set common configurations
 SET 'execution.runtime-mode'            = 'streaming';
@@ -23,19 +29,24 @@ SET 'execution.planner.planner'         = 'blink';
 SET 'execution.planner.result-mode'     = 'table';
 
 
+-- make insfinflow
 -- Execute script 4: 
--- Insert out data into output table, with Pyflink embedding routine included
+-- Insert our data into output table, with Pyflink embedding routine included
+-- 
 -- 4.1 or This could possible all be build into the PyFlink Routine that get's deployed onto the c_cdcsource.demog table
 -- This will first need to 'recreate our c_cdcsource.demog* tables as step 1 in the script before executing the 
 -- Insert into () select () from ...
+--
 SOURCE '/creFlinkFlows/scripts/4.1.creInsertsDemog.sql';
 
 
 -- Execute script 5: 
 -- Create Structures as per 3.2
 
+
 -- Execute script 6: 
 -- Execute Insert as per 4.2 
+
 
 -- Execute script 7: 
 -- Execute CTAS as per 4.3
--- devlab/pyflink/scripts/02_register_ah_embed_udfs.sql
+++ devlab/pyflink/scripts/02_register_ah_embed_udfs.sql
@@ -1,6 +1,6 @@
 -- scripts/01_register_ah_embed_udfs.sql
 
 -- Load Python UDF from file
-CREATE TEMPORARY FUNCTION generate_ah_embedding AS 'ah_embed_udf.main' 
+CREATE TEMPORARY FUNCTION generate_ah_embedding AS 'ah_embed_udf.generate_ah_embedding' 
 LANGUAGE PYTHON USING JAR 'file:///pyflink/udfs/ah_embed_udf.py';
 
