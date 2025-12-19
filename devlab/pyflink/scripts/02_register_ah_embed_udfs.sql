fb81ba22d2f59e9621b886c0f00538750dcfd5a8
George Leonard <georgelza@gmail.com>
2025-12-19 18:32:23 +0200

    Added some scafolding in creFlinkFlows/4.1

--- devlab/pyflink/scripts/02_register_ah_embed_udfs.sql
+++ devlab/pyflink/scripts/02_register_ah_embed_udfs.sql
@@ -1,6 +1,6 @@
 -- scripts/01_register_embed_udfs.sql
 
 -- Load Python UDF from file
-CREATE TEMPORARY FUNCTION embed_accountholders AS 'ah_embed_udf.main' 
+CREATE TEMPORARY FUNCTION generate_ah_embedding AS 'ah_embed_udf.main' 
 LANGUAGE PYTHON USING JAR 'file:///pyflink/udfs/ah_embed_udf.py';
 
--- devlab/pyflink/scripts/03_register_txn_embed_udfs.sql
+++ devlab/pyflink/scripts/03_register_txn_embed_udfs.sql
@@ -1,5 +1,5 @@
 -- scripts/02_register_embed_udfs.sql
 
 -- Load Python UDF from file
-CREATE TEMPORARY FUNCTION embed_transactions AS 'txn_embed_udf.main'
+CREATE TEMPORARY FUNCTION generate_txn_embedding AS 'txn_embed_udf.main'
 LANGUAGE PYTHON USING JAR 'file:///pyflink/udfs/txn_embed_udf.py';
