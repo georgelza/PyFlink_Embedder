82f5e16916ad4ddef15e8cc88132e5ecd0c38dd4
George Leonard <georgelza@gmail.com>
2025-12-19 18:17:06 +0200

    Added some scafolding in creFlinkFlows/4.1

--- devlab/creFlinkFlows/scripts/4.1.creInsertsDemog.sql
+++ devlab/creFlinkFlows/scripts/4.1.creInsertsDemog.sql
@@ -10,19 +10,51 @@ USE finflow;
 SET 'execution.checkpointing.interval'   = '60s';
 SET 'table.exec.sink.upsert-materialize' = 'NONE';
 
+-- Recreate the CDC tables inside this session.
+
+SOURCE '/creFlinkFlows/2.1.creCdcDemog.sql'; 
+
 SET 'pipeline.name' = 'Persist into Paimon (finflow): accountholders';
 
 INSERT INTO accountholders 
-    () AS
-SELECT () 
+    (
+     _id                
+    ,nationalid         
+    ,firstname          
+    ,lastname           
+    ,dob                 
+    ,gender             
+    ,children           
+    ,address            
+    ,accounts           
+    ,emailaddress       
+    ,mobilephonenumber  
+    ,embedding_vector
+    ,embedding_dimensions
+) AS
+SELECT (
+     _id                
+    ,nationalid         
+    ,firstname          
+    ,lastname           
+    ,dob                 
+    ,gender             
+    ,children           
+    ,address            
+    ,accounts           
+    ,emailaddress       
+    ,mobilephonenumber  
+    ,embed_accountholders(nationalid, firstname, lastname, dob, gender, children, address, accounts, emailaddress, mobilephonenumber)             
+    ,375
+) 
 FROM c_cdcsource.demog.accountholders;
 
 
-SET 'pipeline.name' = 'Persist into Paimon (finflow): transactions';
+-- SET 'pipeline.name' = 'Persist into Paimon (finflow): transactions';
 
-INSERT INTO transactions 
-    () AS
-SELECT () 
-FROM c_cdcsource.demog.transactions;
+-- INSERT INTO transactions 
+--     () AS
+-- SELECT () 
+-- FROM c_cdcsource.demog.transactions;
 
 -- See 4.2 and 4.3
